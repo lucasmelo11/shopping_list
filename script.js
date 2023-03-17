ecc5d6986bdedb4e5a7beec2c14f0d0952d0f12c
Lucas de Melo <lucas.melo.lmo@gmail.com>
2023-03-17 00:31:49 -0300

    lista de intens com checkbox

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let item = document.getElementById("item");
-
 function addItem() {
+  let fieldItem = document.getElementById("insert-item");
+
   let elInput = document.createElement("input");
-  elInput.setAttribute("type", "text");
+  elInput.setAttribute("type", "checkbox");
+  elInput.setAttribute("id", "item");
 
   let elLabel = document.createElement("label");
-  elLabel.setAttribute("for", "my-item");
-  elLabel.innerText = "my input";
-  elLabel.appendChild(elInput);
+  elLabel.setAttribute("for", "item");
+  elLabel.innerText = fieldItem.value;
 
   let elDiv = document.getElementsByClassName("item-list")[0];
   elDiv.appendChild(elInput);
+  elDiv.appendChild(elLabel);
+
+  fieldItem.value = "";
 
   console.log("clicou");
 }
+
+// terminar função de remover item da lista
+
+let item = document.getElementById("item");
+let div = document.getElementsByClassName("item-list");
+
+item.addEventListener("change", function () {
+  if (item.checked) {
+    item.remove();
+  }
+});
