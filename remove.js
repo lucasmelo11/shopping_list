4703099103bccf300b00373a6a53dca64128a122
Lucas de Melo <lucas.melo.lmo@gmail.com>
2023-08-24 20:06:10 -0300

    Organização da estrutura de arquivos

--- remove.js
+++
@@ -1,8 +0,0 @@
-function removeItem() {
-  const item = document.querySelectorAll('input[type="checkbox"]'); 
-  if (item.checked) {
-    console.log("existe");
-  } else {
-    console.log("não existe");
-  }
-}
